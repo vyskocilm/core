0269c441905ea80a87d6bc418ca9bc79a6204c13
Ivo Raisr <ivosh@ivosh.net>
2026-06-15 12:17:51 +0200

    Remove redundant registration

--- src/main/java/com/otilm/core/config/cache/CacheConfig.java
+++ src/main/java/com/otilm/core/config/cache/CacheConfig.java
@@ -76,12 +76,6 @@ public class CacheConfig {
                 .recordStats()
                 .removalListener(tokenJtiIndex)
                 .build());
-        mgr.registerCustomCache(CREDENTIAL_VERIFICATION_CACHE, Caffeine.newBuilder()
-                .expireAfterWrite(authCacheProperties.ttlMinutes(), TimeUnit.MINUTES)
-                .maximumSize(authCacheProperties.maxSize())
-                .recordStats()
-                .removalListener(secretRefIndex)
-                .build());
 
         mgr.registerCustomCache(CERTIFICATE_CHAIN_CACHE, Caffeine.newBuilder()
                 .expireAfterWrite(certChainProperties.ttlMinutes(), TimeUnit.MINUTES)
