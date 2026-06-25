d70127bdae2f4754df90695e2e48f2fb776a52ee
Ivo Raisr <ivosh@ivosh.net>
2026-06-25 09:41:40 +0200

    Remove unused stuff

--- src/main/java/com/otilm/core/api/ExceptionHandlingAdvice.java
+++ src/main/java/com/otilm/core/api/ExceptionHandlingAdvice.java
@@ -1,12 +1,10 @@
 package com.otilm.core.api;
 
 import com.otilm.api.exception.*;
-import com.otilm.api.interfaces.core.tsp.error.TspException;
 import com.otilm.api.model.common.AuthenticationServiceExceptionDto;
 import com.otilm.api.model.common.ErrorMessageDto;
 import com.otilm.api.model.core.acme.ProblemDocument;
 import com.otilm.api.model.core.auth.Resource;
-import com.otilm.core.api.tsp.TspResponseBuilder;
 import com.otilm.core.security.authn.PlatformAuthenticationException;
 import com.otilm.core.security.exception.AuthenticationServiceException;
 import com.otilm.core.util.AuthHelper;
@@ -19,7 +17,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -321,18 +318,6 @@ public class ExceptionHandlingAdvice {
 
         ErrorMessageDto errorMessageDto = ErrorMessageDto.getInstance(errorMessage);
         return ResponseEntity.status(ex.getProblemDetail().getStatus()).body(errorMessageDto);
-    }
-
-    /**
-     * Handler for {@link HttpMediaTypeNotSupportedException}.
-     *
-     * @return
-     */
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-    public ErrorMessageDto handleMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex) {
-        LOG.info("HTTP 415: {}", ex.getMessage());
-        return ErrorMessageDto.getInstance("Content-Type is not supported");
     }
 
     /**
